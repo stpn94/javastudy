97c72ae35460eda356f4d8e5d11f5ab74f3b424f
stpn94 <stpn94@gmail.com>
2021-09-08 13:20:42 +0900

    3번문제 완료

--- practice03/src/main/java/prob06/CalcApp.java
+++ practice03/src/main/java/prob06/CalcApp.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 
 public class CalcApp {
 
+	/*
+	 * 다수의 클래스를 정의해보고 활용하는 연습입니다.
+	 * 
+	 * 사칙연산 (+, -, *, /)만 가능하고 2개의 피연산자만 입력 받는 정수 계산기를 만드세요.
+	 * 
+	 * 각 사칙연산을 수행하는 각 연산 클래스 Add, Sub, Mul, Div 클래스를 만들고 각 클래스는 다음의 필드와 메소드를 가집니다.
+	 */
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
