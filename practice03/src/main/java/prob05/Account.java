f518e610f7233f6a96182679068969e9eb8c77a5
stpn94 <stpn94@gmail.com>
2021-09-08 12:02:49 +0900

    practice03 - 05 문제 해결

--- practice03/src/main/java/prob05/Account.java
+++ practice03/src/main/java/prob05/Account.java
@@ -1,4 +1,47 @@
 package prob05;
 
 public class Account {
+	private String accountNo;
+	private int balance;
+
+	public Account() {
+	}
+
+	public Account(String accountNo) {
+		this.accountNo = accountNo;
+	}
+
+	public Account(String accountNo, int balance) {
+		this.accountNo = accountNo;
+		this.balance = balance;
+	}
+
+	public String getAccountNo() {
+		return accountNo;
+	}
+
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public void save(int i) {
+		// TODO Auto-generated method stub
+		balance += i;
+		System.out.println(accountNo + "계좌에" + i + "만원 입금되습니다.");
+	}
+
+	public void deposit(int i) {
+		// TODO Auto-generated method stub
+		balance -= i;
+		System.out.println(accountNo + "계좌에" + i + "만원 출금되습니다.");
+
+	}
 }
