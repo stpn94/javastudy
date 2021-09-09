e749bff61cadfc142d68f17ee08fcbf3d93a08ba
stpn94 <stpn94@gmail.com>
2021-09-09 11:45:41 +0900

    문제4-5 완료

--- practice04/src/main/java/prob04/Prob04.java
+++ practice04/src/main/java/prob04/Prob04.java
@@ -1,9 +1,15 @@
 package prob04;
 
 public class Prob04 {
-
+	/* 다음은 Employee클래스를 상속받아 Depart클래스를 구현합니다. Depart클래스를 구현할 때 실행결과를 참조합니다.
+	<<주의사항>>
+	1	Employee.java 와 Prob04.java클래스는 수정하지 않습니다.
+	2	Depart클래스에 멤버변수를 String department변수외에 추가하지 않습니다.
+	3	Depart클래스에서 생성자를  구현하지 않으면 감점, getInformation()메서드를 구현하지 않으면 감점.  감점요소가 2개면 오답으로 처리됩니다.
+	4	컴파일이 안되거나 Exception이 발생하는 경우도 무조건 오답으로 처리됩니다.
+	*/
 	public static void main(String[] args) {
-		Employee pt = new Depart( "홍길동", 3000, "개발부" );
+		Employee pt = new Depart("홍길동", 3000, "개발부");
 		pt.getInformation();
 	}
 }
--- practice04/src/main/java/prob05/Prob05.java
+++ practice04/src/main/java/prob05/Prob05.java
@@ -1,12 +1,21 @@
 package prob05;
 
 public class Prob05 {
-
+	/*
+	 * <<실행결과>>
+	 * 
+	 * 낮에는 열심히 일하자! night 오후도 낮과 마찬가지로 일해야 합니다.
+	 * 
+	 * 주의 사항 :
+	 *  1. 주어진 main메소드를 이용하여 테스트 합니다.
+	 *  2. 주어진 Class Base는 절대로 수정하면 안됩니다. 
+	 *  3. 컴파일이 안되거나 Exception 이 발생하는 경우는 무조건 오답으로 처리됩니다.
+	 */
 	public static void main(String[] args) {
 		Base base = new MyBase();
 
 		base.service("낮");
 		base.service("밤");
-		base.service("오후"); 
+		base.service("오후");
 	}
 }
